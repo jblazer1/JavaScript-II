5a4a536e2c75b8b3688c935bd239bc86c8b54e4a
JamesB <jblazer1@yahoo.com>
2019-09-09 16:58:20 -0700

    Callbacks complete

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum", "Gum", "Pencil"];
 
 /* 
 
@@ -38,26 +38,32 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+// const test1 = getLength(items, item => `The length of the array is ${item}`);
+// console.log(test1);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item));
 }
 
 /* STRETCH PROBLEM */
@@ -66,4 +72,20 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  // const uniqueSet = new Set(array);
+  // const newArray = [...uniqueSet];
+  // return cb(newArray);
+  let newArr = array;
+  let setArr = new Set(newArr);
+  let finalArr = [...setArr];
+  return cb(finalArr);
 }
+
+console.log("hello");
+
+console.log(getLength(items, a => a));
+console.log(last(items, a => a));
+console.log(sumNums(5, 5, a => a));
+console.log(multiplyNums(5, 5, a => a));
+console.log(contains(items, "Gum", a => a));
+console.log(removeDuplicates(items, a => a));
